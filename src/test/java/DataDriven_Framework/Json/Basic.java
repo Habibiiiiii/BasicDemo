b3da300f2a2e8142b3cd92fd785bdbb747748198
harsha <Harshaonlinetutorial@gmail.com>
2023-01-20 14:49:21 +0530

    The status from Asian Time

---
+++ src/test/java/DataDriven_Framework/Json/Basic.java
@@ -0,0 +1,18 @@
+package DataDriven_Framework.Json;
+
+import org.junit.Test;
+
+public class Basic {
+	@Test
+	public void name() {
+		System.out.println("Habiiiii........... WElcome to ....... Dubai");
+
+	}
+
+	@Test
+	public void name_() {
+		System.out.println("never ever i have");
+
+	}
+
+}
